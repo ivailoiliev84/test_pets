80e8428c7b0c7c3916717e457e9133b3629d347e
ivailoiliev84 <ivailo2250@abv.bg>
2022-03-04 21:09:47 +0200

    add css file and back ground color

--- petstagram/accounts/views.py
+++ petstagram/accounts/views.py
@@ -1,12 +1,11 @@
-import profile
-
 from django.contrib.auth import logout, authenticate, login, get_user_model
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import FormView, CreateView, ListView, DetailView
+from django.views.generic import FormView, CreateView, ListView
 
 from petstagram.accounts.forms import PetstagramUserRegisterForm, PetstagramLoginForm, ProfileCreateForm, \
     ProfileUpdateForm
@@ -79,36 +78,52 @@ def signout_user(request):
 #         return context
 
 
-class ProfilePetsList(ListView):
+class ProfilePetsList(LoginRequiredMixin, ListView):
     template_name = 'profile_templates/profile_pets.html'
     model = Profile
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
         pets = Pet.objects.filter(user_id=self.request.user.id)
-        context['profile'] = Profile.objects.get(pk=self.request.user.id)
+        user_profile = Profile.objects.get(pk=self.request.user.id)
+
+        context['profile'] = user_profile
         context['pets'] = pets
         context['total_pets'] = len(pets)
         return context
 
 
-@login_required
-def profile_details(request):
-    profile = Profile.objects.get(pk=request.user.id)
-    pets = Pet.objects.filter(user_id=request.user.id)
+# @login_required
+# def profile_details(request):
+#     profile = Profile.objects.get(pk=request.user.id)
+#     pets = Pet.objects.filter(user_id=request.user.id)
+#
+#     profile_pets = Pet.objects.filter(user_id=request.user.id).count()
+#
+#     # pet_photo_like = sum(pp.like for pp in PetPhoto.objects.filter(tagged_pet__user_profile=profile).distinct())
+#
+#     context = {
+#         'profile': profile,
+#         'profile_pets': profile_pets,
+#         # 'pet_photo_like': pet_photo_like,
+#         'pets': pets,
+#
+#     }
+#     return render(request, 'profile_templates/profile_details.html', context)
 
-    profile_pets = Pet.objects.filter(user_id=request.user.id).count()
 
-    # pet_photo_like = sum(pp.like for pp in PetPhoto.objects.filter(tagged_pet__user_profile=profile).distinct())
+class ProfileDetailsView(ListView):
+    model = Profile
+    template_name = 'profile_templates/profile_details.html'
 
-    context = {
-        'profile': profile,
-        'profile_pets': profile_pets,
-        # 'pet_photo_like': pet_photo_like,
-        'pets': pets,
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pets = Pet.objects.filter(user_id=self.request.user.id)
+        user_profile = Profile.objects.get(pk=self.request.user.id)
 
-    }
-    return render(request, 'profile_templates/profile_details.html', context)
+        context['profile'] = user_profile
+        context['profile_pets'] = len(pets)
+        return context
 
 
 # class CreateProfile(CreateView):
@@ -135,13 +150,41 @@ def create_profile(request):
     return render(request, 'profile_templates/profile_create.html', context)
 
 
+# class ProfileEditView(LoginRequiredMixin, FormView):
+#     form_class = ProfileUpdateForm
+#     template_name = 'profile_templates/profile_edit.html'
+#     success_url = reverse_lazy('profile')
+#     object = None
+
+    # def get(self, request, *args, **kwargs):
+    #     self.object = Profile.objects.get(pk=self.request.user.id)
+    #     return super().get(request, *args, **kwargs)
+    #
+    # def post(self, request, *args, **kwargs):
+    #     self.object = Profile.objects.get(pk=self.request.user.id)
+    #     return super().get(request, *args, **kwargs)
+
+    # def form_valid(self, form):
+    #     self.object.profile_picture = form.cleaned_data['profile_picture']
+    #     self.object.save()
+    #     return super().form_valid(form)
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['profile'] = self.object
+    #     context['form'] = ProfileUpdateForm(instance=self.object)
+    #
+    #     return context
+
+
+
 def edit_profile(request):
     profile = Profile.objects.get(pk=request.user.id)
     if request.method == "POST":
         form = ProfileUpdateForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
             form.save()
-            return redirect('profile user')
+            return redirect('profile')
 
     else:
         form = ProfileUpdateForm(instance=profile)
