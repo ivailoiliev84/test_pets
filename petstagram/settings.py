77528bdc33aefd9b0442e5347258287b74f76de9
ivailoiliev84 <ivailo2250@abv.bg>
2022-03-05 12:24:29 +0200

    add

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -101,7 +101,6 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 #     }
 # }
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -146,12 +145,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    BASE_DIR / 'static/',
-)
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
