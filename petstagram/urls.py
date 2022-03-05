04e75ae843f4d1d4e2b51b6934c44c5deb5722d2
ivailoiliev84 <ivailo2250@abv.bg>
2022-03-05 13:19:26 +0200

    add

--- petstagram/urls.py
+++ petstagram/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
     path('acconts/', include('petstagram.accounts.urls')),
 
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
