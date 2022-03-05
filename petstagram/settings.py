33a0e2c261126d8d673f3407eb37cfe030960893
ivailoiliev84 <ivailo2250@abv.bg>
2022-03-05 11:01:56 +0200

    add

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dnx3wqh&wx%xo@^2!@yc0ae4&)t(qxam!+sorz0jt=604#gakx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
